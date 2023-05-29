3aa16cbcbde968b31259cce02c880539c2a9a36d
João Guilherme  Araujo Canuto <joaogacanuto@gmail.com>
2023-05-29 13:58:09 -0300

    fix bug em genreateKey

--- Projeto/funcoes.c
+++ Projeto/funcoes.c
@@ -6,5 +6,5 @@
 /* Função padrao para gerar uma chave padrao para o compartilhado usando a função ftkok
 para gerar uma chave unica para aquele arquivo.*/
 key_t generateKey() {
-    return ftok("shared_memory_key", 1) // ftok() = retorna uma chave unica para o arquivo
+    return ftok("shared_memory_key", 1); // ftok() = retorna uma chave unica para o arquivo
 }
